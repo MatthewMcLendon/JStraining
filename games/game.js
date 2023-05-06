4ca35b9a098f4f8f5938de6af54cd751a76d1171
Matthew McLendon <matthewmclendon1006@gmail.com>
2023-05-06 16:25:49 -0500

    added styling, additional text to game

--- games/game.js
+++ games/game.js
@@ -2,10 +2,12 @@ const GameComponent = (game) => {
   return `
     <div class="game-container">
       <div class="game-name">${game.name}</div>
-      <div class="game-genre">${game.genre}</div>
-      <div class="game-players">${game.players}</div>
-      <div class="game-rating">${game.rating}</div>
-      <div class="game-systems">${game.systems}</div>
+      <div class="game-genre">Genre: ${game.genre}</div>
+      <div class="game-players">Number of players: ${game.players}</div>
+      <div class="game-rating">Rating: ${game.rating}/5</div>
+      <div class="game-systems">Available on: ${game.systems.map((game) => {
+        return ` ${game}`;
+      })}</div>
     </div>
     `;
 };
